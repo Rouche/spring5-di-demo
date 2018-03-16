336f3957b3da8f95136674e55b27acb258770ff3
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-03-16 15:46:28 -0400

    Section 33

--- src/main/java/com/resolutech/controllers/ConstructorInjectedController.java
+++ src/main/java/com/resolutech/controllers/ConstructorInjectedController.java
@@ -1,10 +1,12 @@
 package com.resolutech.controllers;
 
 import com.resolutech.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
--- src/main/java/com/resolutech/controllers/SetterInjectedController.java
+++ src/main/java/com/resolutech/controllers/SetterInjectedController.java
@@ -1,17 +1,22 @@
 package com.resolutech.controllers;
 
 import com.resolutech.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class SetterInjectedController {
+
     private GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
     }
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
