bf5d029ad830a9708df91e0aebe5dd5876cb7941
Jean-Francois Larouche <jean-francois.larouche@dti.ulaval.ca>
2018-03-20 15:01:48 -0400

    Section 62

--- src/main/java/com/resolutech/DiDemoApplication.java
+++ src/main/java/com/resolutech/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.resolutech.controllers.ConstructorInjectedController;
 import com.resolutech.controllers.MyController;
 import com.resolutech.controllers.PropertyInjectedController;
 import com.resolutech.controllers.SetterInjectedController;
+import com.resolutech.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,9 +19,15 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
+		FakeDataSource ds = ctx.getBean(FakeDataSource.class);
+
+		System.out.println(ds.getPassword());
+
+/*
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+*/
 	}
 }
---
+++ src/main/java/com/resolutech/config/PropertyConfig.java
@@ -0,0 +1,42 @@
+package com.resolutech.config;
+
+import com.resolutech.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${resolutech.username}")
+    String user;
+
+    @Value("${resolutech.password}")
+    String password;
+
+    @Value("${resolutech.dburl}")
+    String url;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return placeholderConfigurer;
+    }
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(env.getProperty("PASSWORD"));
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+}
